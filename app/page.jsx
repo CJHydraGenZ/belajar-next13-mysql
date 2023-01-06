e4d79c1ce33b89223ed2868227036db1d25a355a
Cahya <cahyasaga321@gmail.com>
2023-01-06 13:31:02 +0800

    styling tailwind form login & register

--- app/page.jsx
+++ app/page.jsx
@@ -10,16 +10,16 @@ import { redirect } from "next/navigation";
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    redirect("/login");
-    // return (
-    //   <>
-    //     Signed in as {session.user.email} <br />
-    //     <button onClick={() => signOut()}>Sign out</button>
-    //   </>
-    // );
-  }
+  // const { data: session } = useSession();
+  // if (!session) {
+  //   redirect("/login");
+  //   // return (
+  //   //   <>
+  //   //     Signed in as {session.user.email} <br />
+  //   //     <button onClick={() => signOut()}>Sign out</button>
+  //   //   </>
+  //   // );
+  // }
   return (
     <main>
       <h1>{session?.user?.email}</h1>
